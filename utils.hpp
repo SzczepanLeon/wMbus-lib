089f978df7ed70c2f5138850eea15f999bb639e8
Szczepan <szczepan.staszak@gmail.com>
2023-04-25 15:24:58 +0200

    1.0.1

--- utils.hpp
+++ utils.hpp
@@ -7,12 +7,16 @@
 
 #include <Arduino.h>
 #include <stdint.h>
-#include<vector>
+#include <vector>
 
 //----------------------------------------------------------------------------------
 //  Function Declareration
 //----------------------------------------------------------------------------------
 void dumpHex(uint8_t* data, uint8_t len, bool newLine = true);
 unsigned char *safeButUnsafeVectorPtr(std::vector<unsigned char> &v);
+bool decrypt_TPL_AES_CBC_IV(vector<unsigned char> &frame, vector<unsigned char>::iterator &pos,
+                            vector<unsigned char> &key, unsigned char *iv,
+                            int *num_encrypted_bytes,
+                            int *num_not_encrypted_at_end);
 
 #endif
