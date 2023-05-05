ee233664ace1c5072d386af83e50dbb575b937a0
Szczepan <szczepan.staszak@gmail.com>
2023-05-05 21:17:05 +0200

    Add C1 mode - part2

--- rf_mbus.hpp
+++ rf_mbus.hpp
@@ -43,18 +43,6 @@
 #define FIXED_PACKET_LENGTH        0x00
 #define INFINITE_PACKET_LENGTH     0x02
 
-typedef struct RXinfoDescr {
-  uint8_t  lengthField;         // The L-field in the WMBUS packet
-  uint16_t length;              // Total number of bytes to to be read from the RX FIFO
-  uint16_t bytesLeft;           // Bytes left to to be read from the RX FIFO
-  uint8_t *pByteIndex;          // Pointer to current position in the byte array
-  bool start;                   // Start of Packet
-  bool complete;                // Packet received complete
-  uint8_t state;
-  WmBusFrameMode framemode;
-  WmBusFrameType frametype;
-} RXinfoDescr;
-
 enum WmBusFrameType : uint8_t {
   WMBUS_FRAME_UNKNOWN = 0,
   WMBUS_FRAMEA = 1,
@@ -66,6 +54,18 @@ enum WmBusFrameMode : uint8_t {
   WMBUS_T1_MODE = 1,
   WMBUS_C1_MODE = 2,
 };
+
+typedef struct RXinfoDescr {
+  uint8_t  lengthField;         // The L-field in the WMBUS packet
+  uint16_t length;              // Total number of bytes to to be read from the RX FIFO
+  uint16_t bytesLeft;           // Bytes left to to be read from the RX FIFO
+  uint8_t *pByteIndex;          // Pointer to current position in the byte array
+  bool start;                   // Start of Packet
+  bool complete;                // Packet received complete
+  uint8_t state;
+  WmBusFrameMode framemode;
+  WmBusFrameType frametype;
+} RXinfoDescr;
 
 typedef struct WMbusFrame {
   std::vector<unsigned char> frame{};
