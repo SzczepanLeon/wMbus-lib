7e638bbae9d21b677190de1a9f7abd9f709b03fd
Jacman777 <149330119+Jacman777@users.noreply.github.com>
2023-12-17 09:12:05 +0100

    Update rf_mbus.cpp

--- rf_mbus.cpp
+++ rf_mbus.cpp
@@ -374,7 +374,7 @@ bool rf_mbus::task() {
         }
         Serial.println("");
         //rxStatus = PACKET_UNKNOWN_ERROR;
-        rxLength = RXinfo.lengthField + 2;
+        rxLength = RXinfo.lengthField;
         rxStatus = verifyCrcBytesCmodeB_local(this->MBbytes + 2, this->MBpacket, rxLength);
         rxStatus = PACKET_OK;
       }
