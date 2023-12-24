31e2672d8f341bb17f43fd055f8e9e58515f7758
Jacman777 <149330119+Jacman777@users.noreply.github.com>
2023-12-24 23:47:49 +0100

    Update rf_mbus.cpp

--- rf_mbus.cpp
+++ rf_mbus.cpp
@@ -73,7 +73,7 @@ uint8_t rf_mbus::start(bool force) {
 WMbusFrame rf_mbus::get_frame() {
   std::vector<unsigned char> frame;
   if (RXinfo.frametype == WMBUS_FRAMEB){
-    std::vector<unsigned char> frame(this->MBpacket, this->MBpacket + this->MBpacket[0]+1);
+    std::vector<unsigned char> frame(this->MBpacket, this->MBpacket + this->MBpacket[0]);
   }
   else{
     uint8_t len_without_crc = crcRemove(this->MBpacket, packetSize(this->MBpacket[0]));
