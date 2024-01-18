002ef98d0c880f2dfad52ce31cc52225e5ba0449
Szczepan <szczepan.staszak@gmail.com>
2024-01-18 17:31:13 +0100

    Send full frame

--- rf_mbus.cpp
+++ rf_mbus.cpp
@@ -72,7 +72,9 @@ uint8_t rf_mbus::start(bool force) {
 
 WMbusFrame rf_mbus::get_frame() {
   // uint8_t len_without_crc = crcRemove(this->MBpacket, packetSize(this->MBpacket[0]));
-  std::vector<unsigned char> frame(this->MBpacket, this->MBpacket + packetSize(this->MBpacket[0]));
+  // std::vector<unsigned char> frame(this->MBpacket, this->MBpacket + packetSize(this->MBpacket[0]));
+  std::vector<unsigned char> frame(this->MBbytes, this->MBbytes + RXinfo.length);
+
   this->returnFrame.frame = frame;
   return this->returnFrame;
 }
