11e9d2ad17f4f212f160863ad298df017257f529
Szczepan <szczepan.staszak@gmail.com>
2024-01-19 18:50:09 +0100

    Traces 11

--- rf_mbus.hpp
+++ rf_mbus.hpp
@@ -22,7 +22,7 @@
 
 static const char *TAG_L = "wmbus-lib";
 
-using namespace esphome;
+// using namespace esphome;
 
 //----------------------------------------------------------------------------------
 //  Constants 
@@ -187,7 +187,7 @@ uint8_t rf_mbus::start(bool force) {
 }
 
 WMbusFrame rf_mbus::get_frame() {
-  // ESP_LOGVV(TAG_L, "get_frame()");
+  esphome::ESP_LOGVV(TAG_L, "get_frame()");
   // uint8_t len_without_crc = crcRemove(this->MBpacket, packetSize(this->MBpacket[0]));
   // std::vector<unsigned char> frame(this->MBpacket, this->MBpacket + packetSize(this->MBpacket[0]));
   std::vector<unsigned char> frame(this->MBbytes, this->MBbytes + RXinfo.length);
