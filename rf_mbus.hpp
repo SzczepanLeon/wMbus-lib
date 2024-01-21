213ada5ab526607ee5d5debf9cd88ad875c6021b
Szczepan <szczepan.staszak@gmail.com>
2024-01-21 20:11:18 +0100

    Traces 34

--- rf_mbus.hpp
+++ rf_mbus.hpp
@@ -30,57 +30,6 @@ static const char *TAG_L = "wmbus-lib";
 #include <string>
 #include <vector>
 
-static char format_my_hex_char(uint8_t v) { return v >= 10 ? 'a' + (v - 10) : '0' + v; }
-std::string format_my_hex(const uint8_t *data, size_t length) {
-  std::string ret;
-  ret.resize(length * 2);
-  for (size_t i = 0; i < length; i++) {
-    ret[2 * i] = format_my_hex_char((data[i] & 0xF0) >> 4);
-    ret[2 * i + 1] = format_my_hex_char(data[i] & 0x0F);
-  }
-  return ret;
-}
-std::string format_my_hex(const std::vector<uint8_t> &data) { return format_my_hex(data.data(), data.size()); }
-
-static char format_my_hex_pretty_char(uint8_t v) { return v >= 10 ? 'A' + (v - 10) : '0' + v; }
-std::string format_my_hex_pretty(const uint8_t *data, size_t length) {
-  if (length == 0)
-    return "";
-  std::string ret;
-  ret.resize(3 * length - 1);
-  for (size_t i = 0; i < length; i++) {
-    ret[3 * i] = format_my_hex_pretty_char((data[i] & 0xF0) >> 4);
-    ret[3 * i + 1] = format_my_hex_pretty_char(data[i] & 0x0F);
-    if (i != length - 1)
-      ret[3 * i + 2] = '.';
-  }
-  if (length > 4)
-    return ret + " (" + std::to_string(length) + ")";
-  return ret;
-}
-std::string format_my_hex_pretty(const std::vector<uint8_t> &data) { return format_my_hex_pretty(data.data(), data.size()); }
-
-std::string format_my_hex_pretty(const uint16_t *data, size_t length) {
-  if (length == 0)
-    return "";
-  std::string ret;
-  ret.resize(5 * length - 1);
-  for (size_t i = 0; i < length; i++) {
-    ret[5 * i] = format_my_hex_pretty_char((data[i] & 0xF000) >> 12);
-    ret[5 * i + 1] = format_my_hex_pretty_char((data[i] & 0x0F00) >> 8);
-    ret[5 * i + 2] = format_my_hex_pretty_char((data[i] & 0x00F0) >> 4);
-    ret[5 * i + 3] = format_my_hex_pretty_char(data[i] & 0x000F);
-    if (i != length - 1)
-      ret[5 * i + 2] = '.';
-  }
-  if (length > 4)
-    return ret + " (" + std::to_string(length) + ")";
-  return ret;
-}
-std::string format_my_hex_pretty(const std::vector<uint16_t> &data) { return format_my_hex_pretty(data.data(), data.size()); }
-
-////
-
 //----------------------------------------------------------------------------------
 //  Constants 
 //----------------------------------------------------------------------------------
@@ -152,6 +101,60 @@ typedef struct WMbusFrame {
 
 class rf_mbus {
   public:
+
+//
+
+static char format_my_hex_char(uint8_t v) { return v >= 10 ? 'a' + (v - 10) : '0' + v; }
+std::string format_my_hex(const uint8_t *data, size_t length) {
+  std::string ret;
+  ret.resize(length * 2);
+  for (size_t i = 0; i < length; i++) {
+    ret[2 * i] = format_my_hex_char((data[i] & 0xF0) >> 4);
+    ret[2 * i + 1] = format_my_hex_char(data[i] & 0x0F);
+  }
+  return ret;
+}
+std::string format_my_hex(const std::vector<uint8_t> &data) { return format_my_hex(data.data(), data.size()); }
+
+static char format_my_hex_pretty_char(uint8_t v) { return v >= 10 ? 'A' + (v - 10) : '0' + v; }
+std::string format_my_hex_pretty(const uint8_t *data, size_t length) {
+  if (length == 0)
+    return "";
+  std::string ret;
+  ret.resize(3 * length - 1);
+  for (size_t i = 0; i < length; i++) {
+    ret[3 * i] = format_my_hex_pretty_char((data[i] & 0xF0) >> 4);
+    ret[3 * i + 1] = format_my_hex_pretty_char(data[i] & 0x0F);
+    if (i != length - 1)
+      ret[3 * i + 2] = '.';
+  }
+  if (length > 4)
+    return ret + " (" + std::to_string(length) + ")";
+  return ret;
+}
+std::string format_my_hex_pretty(const std::vector<uint8_t> &data) { return format_my_hex_pretty(data.data(), data.size()); }
+
+std::string format_my_hex_pretty(const uint16_t *data, size_t length) {
+  if (length == 0)
+    return "";
+  std::string ret;
+  ret.resize(5 * length - 1);
+  for (size_t i = 0; i < length; i++) {
+    ret[5 * i] = format_my_hex_pretty_char((data[i] & 0xF000) >> 12);
+    ret[5 * i + 1] = format_my_hex_pretty_char((data[i] & 0x0F00) >> 8);
+    ret[5 * i + 2] = format_my_hex_pretty_char((data[i] & 0x00F0) >> 4);
+    ret[5 * i + 3] = format_my_hex_pretty_char(data[i] & 0x000F);
+    if (i != length - 1)
+      ret[5 * i + 2] = '.';
+  }
+  if (length > 4)
+    return ret + " (" + std::to_string(length) + ")";
+  return ret;
+}
+std::string format_my_hex_pretty(const std::vector<uint16_t> &data) { return format_my_hex_pretty(data.data(), data.size()); }
+
+//
+
     bool init(uint8_t mosi, uint8_t miso, uint8_t clk, uint8_t cs,
               uint8_t gdo0, uint8_t gdo2, float freq) {
   bool retVal = false;
